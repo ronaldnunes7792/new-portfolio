8f84844c8afc7cb7a30c6f677daa6319ad09a9ce
ronaldnunes7792 <leewenming17160@gmail.com>
2025-04-28 19:01:14 -0400

    edited upload button

--- components/intro.tsx
+++ components/intro.tsx
@@ -15,6 +15,13 @@ export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
   const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
 
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = "Ronald-Nunes-Resume.pdf"; // URL or path to the file
+    link.download = "resume.pdf"; // Specify the filename for the download
+    link.click(); // Trigger the download
+  };
+
   return (
     <section
       ref={ref}
@@ -116,7 +123,14 @@ export default function Intro() {
           <BsArrowDown className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
 
-        <a
+        <button
+          onClick={handleDownload}
+          className="px-6 py-2 w-full border-1 border-white-transparent sm:w-auto rounded-full mr-0 sm:mr-4 bg-[#FFF] text-black text-lg font-medium hover:bg-custom-text text-center"
+        >
+          Download Resume
+        </button>
+
+        {/* <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
           href={profile.resumeLink}
           target="_blank"
@@ -125,7 +139,7 @@ export default function Intro() {
         >
           Download Resume{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
-        </a>
+        </a> */}
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
